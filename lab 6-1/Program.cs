c9fb7c79a0b16ab990e83254e6e4efdf3aa44853
Tatyana <Tatyana@DESKTOP-74DNP3C>
2021-12-15 20:49:20 +0300

    Добавьте файлы проекта.

---
+++ lab 6-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_6_1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+            string[] str = input.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            int maxlen = 0, index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > maxlen)
+                {
+                    maxlen = str[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово: {0}", str[index]);
+            Console.ReadLine();
+        }
+    }
+}
